c259e31c42c6d35d1421f490e0d3b9039e56b7bc
sarAmgad <sarahelmy56@gmail.com>
2022-05-19 21:50:12 +0200

    model

---
+++ model.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd  
+import numpy as np
+# import matplotib.pyplot as plt
+
+
+# In[2]:
+
+
+cardio_f = pd.read_csv('G:/SBME/Secound year/Secound Term/BioStatistics/Project/Data/Cardio.csv')
+cardio_f.head()
+cardio_f.tail()
+cardio_f.size
+cardio_f.count()
+cardio_f['cardio'].value_counts()
+
+
+# In[3]:
+
+
+positive_f = cardio_f[cardio_f['cardio']==1] [0:200]
+negative_f = cardio_f[cardio_f['cardio']==0] [0:200]
+
+# positive_f.plot(kind = 'scatter' )
+
+
+# In[4]:
+
+
+cardio_f.dtypes
+
+# cardio_f = cardio_f[pd.to_numeric(cardio_f['weight'], errors = 'coerce').notnull()]
+# cardio_f['weight'] = cardio_f ['weight'].astype('int')
+# cardio_f.dtypes
+
+
+# In[5]:
+
+
+cardio_f.columns
+
+feature_f = cardio_f [['age', 'gender', 'height', 'weight', 'ap_hi', 'ap_lo',
+       'cholesterol', 'gluc', 'smoke', 'alco', 'active']]
+
+x = np.asarray(feature_f)
+
+y = np.asarray(cardio_f['cardio'])
+
+y[0:5]
+
+
+# In[7]:
+
+
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 4)
+
+# (55440, 11)
+X_train.shape
+
+# (55440,)
+y_train.shape
+
+# (13861, 11)
+X_test.shape
+
+# (13861,)
+y_test.shape
+
+
+# In[8]:
+
+
+from sklearn import svm
+
+classifier = svm.SVC(kernel = 'linear', gamma = 'auto', C = 1)
+classifier.fit(X_train, y_train)
+
+y_predict = classifier.predict(X_test)
+
+
+# In[12]:
+
+
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score, classification_report 
+
+
+print(classification_report(y_test, y_predict))
+
+# print(classification_report(y_test, y_predict))
+print('Predicted labels: ', y_predict)
+print('Accuracy: ', accuracy_score(y_test, y_predict))
+
+
+# In[ ]:
+
+
+
+
