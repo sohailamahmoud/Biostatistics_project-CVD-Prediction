fa289ce870b7fa591c6632bf9cfd915c268519ef
sarAmgad <sarahelmy56@gmail.com>
2022-05-20 15:49:47 +0200

    no changes (comments)

--- model.py
+++ model.py
@@ -56,16 +56,16 @@ from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.8, random_state = 4)
 
-# (55440, 11)
+# (13860, 11)
 X_train.shape
 
-# (55440,)
+# (13860, 1)
 y_train.shape
 
-# (13861, 11)
+# (55441, 11)
 X_test.shape
 
-# (13861,)
+# (55441, 1)
 y_test.shape
 
 
