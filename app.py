a5c1336a5df98bb053203989cd335ace305700df
sarAmgad <sarahelmy56@gmail.com>
2022-05-21 22:18:45 +0200

    last

---
+++ app.py
@@ -0,0 +1,56 @@
+from pyexpat import model
+from xml.etree.ElementInclude import include
+from click import style
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+
+submission = []
+
+def home():
+    if request.method== "POST":
+        print(request.form)
+        submission.append(
+                ( 
+             request.form.get("age" * 365),
+             request.form.get("gender"),
+             request.form.get("height"),
+             request.form.get("weight"),
+             request.form.get("AP-HI"),
+             request.form.get("AP-LO"),
+             request.form.get("Cholesterol"),
+             request.form.get("Glucose"),
+             request.form.get("smoke"),
+             request.form.get("alcohol"),
+             request.form.get("Active"),
+                 )
+                       )
+    return render_template("index.html")
+
+app = Flask(__name__)
+
+model = pickle.load(open("model.pkl", "rb"))
+
+@app.route("/", methods=["GET","POST"])
+def home():
+    if request.method == "POST":
+        print(request.form)
+    return render_template("index.html")
+
+@app.route("/predict", methods=["GET","POST"])
+def show_predict():
+
+    int_features = [x for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    
+    if prediction == 1:
+        prediction_text = "cardio"
+    elif prediction == 0:
+        prediction_text = "not"
+
+    return render_template("index.html", prediction_text = "The prediction is {}".format(prediction_text))
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- model.py
+++ model.py
@@ -6,23 +6,37 @@
 import pandas as pd  
 import numpy as np
 import pickle
-from sklearn.model_selection import train_test_split
-from sklearn import svm
-from sklearn.metrics import accuracy_score, classification_report 
-
 
 # In[2]:
 
 
-# Reading the csv file
 cardio_f = pd.read_csv("cardio_train.csv")
 cardio_f.head()
+cardio_f.tail()
+cardio_f.size
+cardio_f.count()
+cardio_f['cardio'].value_counts()
+
+
+# In[3]:
+
+
+positive_f = cardio_f[cardio_f['cardio']==1] [0:200]
+negative_f = cardio_f[cardio_f['cardio']==0] [0:200]
+
+
+# In[4]:
+
+
+cardio_f.dtypes
+
 
 
 # In[5]:
 
 
-# Removing id & cardio
+cardio_f.columns
+
 feature_f = cardio_f [['age', 'gender', 'height', 'weight', 'ap_hi', 'ap_lo',
        'cholesterol', 'gluc', 'smoke', 'alco', 'active']]
 
@@ -30,23 +44,35 @@ x = np.asarray(feature_f)
 
 y = np.asarray(cardio_f['cardio'])
 
+y[0:5]
 
 
 # In[7]:
 
 
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.9, random_state = 4)
 
-'''
-Train sample: 6930
-Test sample: 62371
-'''
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.8, random_state = 4)
+
+# (13860, 11)
+X_train.shape
+
+# (13860, 1)
+y_train.shape
+
+# (55441, 11)
+X_test.shape
+
+# (55441, 1)
+y_test.shape
 
 
 # In[8]:
 
 
-# Creating the model
+from sklearn import svm
+
 classifier = svm.SVC(kernel = 'linear', gamma = 'auto', C = 1)
 classifier.fit(X_train, y_train)
 
@@ -54,13 +80,20 @@ y_predict = classifier.predict(X_test)
 
 
 # In[12]:
- 
 
-print('Predicted labels: ', y_predict)
+
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score, classification_report 
+
+
+# print(classification_report(y_test, y_predict))
+print('Predicted labels: ', y_predict[0:10])
 print('Accuracy: ', accuracy_score(y_test, y_predict))
 
+pickle.dump(classifier, open("model.pkl","wb"))
 
 # In[ ]:
 
-pickle.dump(cardio_f, open('finalized_model.sav', 'wb'))
 
+
+# %%
